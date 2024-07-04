28318f8fc9376deeacfccf610a700198354004a5
mainformatico <magutierrezsistemas@gmail.com>
2024-07-04 21:07:13 +0200

    updating

--- bot.py
+++ bot.py
@@ -1,7 +1,10 @@
 import logging
 
 from telegram.ext import Updater, CommandHandler, MessageHandler, filters
-from .checker import checker
+<<<<<<< HEAD
+=======
+from checker import checker
+>>>>>>> 3083944 (updating)
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -12,15 +15,6 @@ logger = logging.getLogger(__name__)
 
 def error(update, context):
     logger.warning('Update "%s" caused error "%s"', update, context.error)
-
-def hola(update, context):
-    update.message.reply_text('Hola, ¿Qué tal estás?')
-
-def hello(update, context):
-    update.message.reply_text('Hi, dude')
-
-def weather(update, context):
-    update.message.reply_text("Check weather on this link (also available in English too): https://www.eltiempo.es")
 
 def temperature(update, context):
     verify = checker()
@@ -35,15 +29,16 @@ def show_help(update, context):
    
     
 def main():
-    updater = Updater("put_your_own_token_here", use_context=True)
+    updater = Updater("your API key here", use_context=True)
 
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler("hola", hola))
-    dp.add_handler(CommandHandler("weather", weather))
     dp.add_handler(CommandHandler("temperature",temperature))
     dp.add_handler(CommandHandler("hosts",hosts))
+<<<<<<< HEAD
     dp.add_handler(CommandHandler("hello",hello))
+=======
+>>>>>>> 3083944 (updating)
     dp.add_handler(CommandHandler("help",show_help))
 
     dp.add_error_handler(error)
